d13778ef0c3b0dc06ec6df65869a9b13f29f08ee
gskgrek <biuro@greku.pl>
2024-06-15 21:45:08 +0200

    Update stream page with loading.tsx

--- src/app/stream/page.tsx
+++ src/app/stream/page.tsx
@@ -1,13 +1,10 @@
 import { AlbumsList } from "@/components/albums/AlbumsList"
-import { Suspense } from "react"
 
 const Stream = () => {
   return (
     <div>
       <h1>Stream</h1>
-      <Suspense fallback={<div>Loading list</div>}>
-        <AlbumsList />
-      </Suspense>
+      <AlbumsList />
     </div>
   )
 }
