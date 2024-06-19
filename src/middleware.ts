0fc98eaf6464b07d8ed20a359b4c4f0bd018dd15
gskgrek <biuro@greku.pl>
2024-06-19 22:00:29 +0200

    remove unnecessary type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { auth } from '@/auth'
  
-export default auth((req: any) => {
+export default auth(req => {
   if (!req.auth) {
     const newUrl = new URL("/auth", req.nextUrl.origin)
     return Response.redirect(newUrl)
